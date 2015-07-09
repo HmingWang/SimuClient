1693aca91ff58eca2435f62d2407446a367135ea
Xest <whaim1986@gmail.com>
2015-07-09 09:18:36 +0800

    init

---
+++ client/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxCommand = new System.Windows.Forms.TextBox();
+            this.tbxIP = new System.Windows.Forms.TextBox();
+            this.tbxPort = new System.Windows.Forms.TextBox();
+            this.rtbMsg = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxChar = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbxCommand
+            // 
+            this.tbxCommand.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbxCommand.Location = new System.Drawing.Point(84, 326);
+            this.tbxCommand.Name = "tbxCommand";
+            this.tbxCommand.Size = new System.Drawing.Size(461, 21);
+            this.tbxCommand.TabIndex = 0;
+            // 
+            // tbxIP
+            // 
+            this.tbxIP.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tbxIP.Location = new System.Drawing.Point(85, 358);
+            this.tbxIP.Name = "tbxIP";
+            this.tbxIP.Size = new System.Drawing.Size(100, 21);
+            this.tbxIP.TabIndex = 1;
+            this.tbxIP.Text = "127.0.0.1";
+            // 
+            // tbxPort
+            // 
+            this.tbxPort.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tbxPort.Location = new System.Drawing.Point(254, 358);
+            this.tbxPort.MaxLength = 4;
+            this.tbxPort.Name = "tbxPort";
+            this.tbxPort.Size = new System.Drawing.Size(53, 21);
+            this.tbxPort.TabIndex = 2;
+            this.tbxPort.Text = "1986";
+            // 
+            // rtbMsg
+            // 
+            this.rtbMsg.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbMsg.BackColor = System.Drawing.SystemColors.Desktop;
+            this.rtbMsg.ForeColor = System.Drawing.Color.Lime;
+            this.rtbMsg.Location = new System.Drawing.Point(12, 31);
+            this.rtbMsg.Name = "rtbMsg";
+            this.rtbMsg.Size = new System.Drawing.Size(625, 287);
+            this.rtbMsg.TabIndex = 3;
+            this.rtbMsg.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(551, 326);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "发送";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(317, 358);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "连接";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 331);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "发送命令：";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 363);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "IP：";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(208, 364);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "端口：";
+            // 
+            // tbxChar
+            // 
+            this.tbxChar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tbxChar.Location = new System.Drawing.Point(599, 359);
+            this.tbxChar.MaxLength = 1;
+            this.tbxChar.Name = "tbxChar";
+            this.tbxChar.Size = new System.Drawing.Size(21, 21);
+            this.tbxChar.TabIndex = 9;
+            this.tbxChar.Text = ";";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(546, 364);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "分隔符：";
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.Location = new System.Drawing.Point(398, 359);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "断开";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "消息记录：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 399);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbxChar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rtbMsg);
+            this.Controls.Add(this.tbxPort);
+            this.Controls.Add(this.tbxIP);
+            this.Controls.Add(this.tbxCommand);
+            this.Name = "Form1";
+            this.Text = "测试客户端模拟器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxCommand;
+        private System.Windows.Forms.TextBox tbxIP;
+        private System.Windows.Forms.TextBox tbxPort;
+        private System.Windows.Forms.RichTextBox rtbMsg;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxChar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ client/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace client
+{
+    public partial class Form1 : Form
+    {
+        private Socket socketClient;
+        private delegate void dlgtFunc(string s);
+        private byte[] obuff;
+        private byte[] ibuff;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (socketClient!=null&& socketClient.Connected) {
+                log("服务器已连接！");
+                return;
+            }
+            string ip = tbxIP.Text;
+            string port = tbxPort.Text;
+            socketClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                socketClient.Connect(ip, int.Parse(port));
+                log("连接服务器成功：" + ip + port);
+                
+                Thread t = new Thread(do_read);
+                t.IsBackground = true;
+                t.Start();
+            }
+            catch(Exception ex) {
+                log("连接服务器失败！错误信息：" + ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string comm = tbxCommand.Text;
+            try
+            {
+                List<byte> tmp=new List<byte>(Encoding.Default.GetBytes(tbxCommand.Text));
+                tmp.Add(0);
+                obuff=tmp.ToArray();
+                socketClient.Send(obuff);
+                log("<<" + comm);
+                tbxCommand.Text = "";
+
+            }
+            catch (Exception ex) {
+                log("发送消息失败！错误信息：" + ex.Message);
+                
+            }
+        }
+
+        private void log(string str) {
+            if (rtbMsg.InvokeRequired) 
+            {
+                dlgtFunc dlgtLog = new dlgtFunc(log);
+                rtbMsg.Invoke(dlgtLog, str);
+                return;
+            }
+            rtbMsg.AppendText(str + Environment.NewLine);
+            rtbMsg.ScrollToCaret();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (socketClient!=null&& socketClient.Connected) {
+                socketClient.Close();
+                log("服务器已断开！");
+                return;
+            }
+        }
+
+        private void do_read()
+        {
+            //线程
+            ibuff=new byte[1024];
+            string str;
+            log("数据读取开启...");
+            try
+            {
+                while (socketClient.Connected && socketClient.Receive(ibuff) > 0)
+                {
+                    str=System.Text.Encoding.Default.GetString(ibuff).TrimEnd('\0');
+                    log(">>" + str);
+                    ibuff = new byte[1024];                    
+                }
+            }
+            catch (Exception ex)
+            {
+                log(ex.Message);
+            }
+        }
+
+    }
+}
