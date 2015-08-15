3c5a2d1e4e1bf270c6e84c8a5699827f6d93697c
whaim <whaim@live.com>
2015-08-15 12:29:38 +0800

    5081503

--- client/selectrole.Designer.cs
+++ client/selectrole.Designer.cs
@@ -28,13 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("", 0);
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("", 1);
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("", 3);
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("", 4);
+            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem("", 5);
+            System.Windows.Forms.ListViewItem listViewItem6 = new System.Windows.Forms.ListViewItem("", 6);
+            System.Windows.Forms.ListViewItem listViewItem7 = new System.Windows.Forms.ListViewItem("", 7);
+            System.Windows.Forms.ListViewItem listViewItem8 = new System.Windows.Forms.ListViewItem("", 8);
+            System.Windows.Forms.ListViewItem listViewItem9 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem10 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem11 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem12 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem13 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem14 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem15 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem16 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem17 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem18 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem19 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem20 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem21 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem22 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem23 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem24 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem25 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem26 = new System.Windows.Forms.ListViewItem("");
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(selectrole));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3,
+            listViewItem4,
+            listViewItem5,
+            listViewItem6,
+            listViewItem7,
+            listViewItem8,
+            listViewItem9,
+            listViewItem10,
+            listViewItem11,
+            listViewItem12,
+            listViewItem13,
+            listViewItem14,
+            listViewItem15,
+            listViewItem16,
+            listViewItem17,
+            listViewItem18,
+            listViewItem19,
+            listViewItem20,
+            listViewItem21,
+            listViewItem22,
+            listViewItem23,
+            listViewItem24,
+            listViewItem25,
+            listViewItem26});
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(952, 667);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "曹操.png");
+            this.imageList1.Images.SetKeyName(1, "大乔.png");
+            this.imageList1.Images.SetKeyName(2, "貂蝉.png");
+            this.imageList1.Images.SetKeyName(3, "甘宁.png");
+            this.imageList1.Images.SetKeyName(4, "关羽.png");
+            this.imageList1.Images.SetKeyName(5, "郭嘉.png");
+            this.imageList1.Images.SetKeyName(6, "华佗.png");
+            this.imageList1.Images.SetKeyName(7, "黄盖.png");
+            this.imageList1.Images.SetKeyName(8, "黄月英.png");
+            this.imageList1.Images.SetKeyName(9, "刘备.png");
+            this.imageList1.Images.SetKeyName(10, "陆逊.png");
+            this.imageList1.Images.SetKeyName(11, "吕布.png");
+            this.imageList1.Images.SetKeyName(12, "吕蒙.png");
+            this.imageList1.Images.SetKeyName(13, "马超.png");
+            this.imageList1.Images.SetKeyName(14, "司马懿.png");
+            this.imageList1.Images.SetKeyName(15, "孙权.png");
+            this.imageList1.Images.SetKeyName(16, "孙尚香.png");
+            this.imageList1.Images.SetKeyName(17, "夏侯惇.png");
+            this.imageList1.Images.SetKeyName(18, "许诸.png");
+            this.imageList1.Images.SetKeyName(19, "张飞.png");
+            this.imageList1.Images.SetKeyName(20, "张辽.png");
+            this.imageList1.Images.SetKeyName(21, "赵云.png");
+            this.imageList1.Images.SetKeyName(22, "甄姬.png");
+            this.imageList1.Images.SetKeyName(23, "周瑜.png");
+            this.imageList1.Images.SetKeyName(24, "诸葛亮.png");
             // 
             // selectrole
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(976, 691);
+            this.Controls.Add(this.listView1);
             this.Name = "selectrole";
             this.Text = "角色选择";
             this.ResumeLayout(false);
@@ -42,5 +140,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ImageList imageList1;
     }
 }
